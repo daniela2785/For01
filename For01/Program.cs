e8dd5ae82bb49a8440478fada459c34ac857ce44
daniela marques <dd978452@gmail.com>
2023-02-09 17:30:13 -0300

    Estrutura de Repetições For

---
+++ For01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ExerciciosPropostos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //01
+            /*int N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++){
+                vet[i] = double.Parse(valores[i], CultureInfo.InvariantCulture);
+            }
+
+            double maior = vet[0];
+            int posicao = 0;
+            for(int i = 1 ; i < N; i++){
+                if (vet[i] > maior)
+                {
+                    maior = vet[i];
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maior.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine(posicao);*/
+
+
+        }
+    }
+}
+
